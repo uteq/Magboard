d4c97945890f18ec52179b2160506ec920ec5111
Dennis de Jong <dennis.c.dejong@gmail.com>
2012-12-17 14:13:40 +0100

    Added GHUnit for unit testing

---
+++ MagBoard_v2/MagBoard/MagBoard/Views/Dashboard/DashboardVC.h
@@ -0,0 +1,16 @@
+//
+//  DashboardVC.h
+//  MagBoard
+//
+//  Created by Dennis de Jong on 17-12-12.
+//  Copyright (c) 2012 Dennis de Jong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DashboardVC : UIViewController
+
+-(void)constructHeader;
+-(void)constructTabBar;
+
+@end
